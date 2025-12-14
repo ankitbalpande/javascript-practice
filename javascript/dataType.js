1dab56bdb6349ec5483b29ada26f49e70425f89a
Ankit Balpande <127498035+ankitbalpande@users.noreply.github.com>
2025-12-14 12:03:22 +0000

    datatype

---
+++ javascript/dataType.js
@@ -0,0 +1,20 @@
+// data types of javascript
+
+let name = "Ankit Balpande"
+let age = 26
+let isLoggedIn = true
+
+// datatypes
+
+// number => 2 to pawer 52
+// bigint => long number value is bigint and it is use rearly 
+// string => "" 
+// boolean => true/false
+// null  => null is a standalon value  ye value bhi hai or ek datatypes bhi hai 
+//undefined => jo value assign hi nhi ki ho 
+
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(isLoggedIn))
+console.log(typeof(null))
+console.log(undefined)
---
+++ javascript/variables.js
@@ -0,0 +1,11 @@
+// in a industry var is not use 
+// because of issue in block scope and function scope
+
+const accountId = 987654321 //not changeable value store in const 
+let accountEmail = 'ankit@gmail.com' //value is changeble but not redeclear let 
+let accountStatus;
+var accrounPassword = '1234' 
+acountCity = 'Pandhurna' //its variable but it is not good prectice in javascript
+
+
+console.table([accountId, accountEmail, accrounPassword, acountCity, accountStatus])
