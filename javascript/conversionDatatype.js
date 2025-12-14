6fbd54da87009fdf0d03b58407722682c718f92b
Ankit Balpande <127498035+ankitbalpande@users.noreply.github.com>
2025-12-14 12:42:37 +0000

    conversion data

---
+++ javascript/conversionDatatype.js
@@ -0,0 +1,29 @@
+console.log("ANkit Balpande")
+
+let score = "Ankit"
+console.log(score)//Ankit
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)//NAN
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let loogedIn = "" //if i check on empty string output geting false
+// let loogedIn = "Ankit" // if i check on string output geting true
+// let loogedIn = 0 //if i  check number of 0  on boolean output is false
+let loogedIn = 1 // if i check number of 1 on boolean output is true
+
+let booleanLoogedIn = Boolean(loogedIn)
+console.log(booleanLoogedIn)
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber) // this value is showing is number type but it is string 
+console.log(typeof stringNumber) // see it is a string 
+
+// --------------------------Operations----------------------------
